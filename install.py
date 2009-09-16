9a243db2dbc676d693ac4c582cfe898a60209d3f
David Keogh <davekeogh@shaw.ca>
2009-09-15 20:01:11 -0700

    Back up old files rather than deleting them

--- install.py
+++ install.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import os, os.path
+import os, os.path, shutil
 
 if __name__ == '__main__':
     for file in os.listdir(os.getcwd()):
@@ -13,8 +13,8 @@ if __name__ == '__main__':
         else:
             target = os.path.join(os.path.expanduser('~'), '.%s' % file)
             
-            if os.path.exists(target):
-                os.remove(target)
+            if os.path.isfile(target):
+                shutil.move(target, '%s.bak' % target)
             
             print 'Linking %s to %s...' % (target, file)
             os.symlink(os.path.join(os.getcwd(), file), target)
